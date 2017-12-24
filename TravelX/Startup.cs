7fe5f6e33478f528ea08036052aef2597b8dd3aa
ROD-AWM\Administrator <farhatkhanwazir@gmail.com>
2017-12-24 12:47:09 +0500

    TravelXIntialCode

---
+++ TravelX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelX.Startup))]
+namespace TravelX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
